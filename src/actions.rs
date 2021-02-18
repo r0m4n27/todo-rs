0e37eb17c728a1cd04e8f2f759b3f7795d286989
Roman Kolesnikov <rk2001.mailbox@gmail.com>
2021-02-18 17:02:13 +0100

    Rewrite find_todos: Remove TodoParser struct and use &str for input

--- src/actions.rs
+++ src/actions.rs
@@ -1,10 +1,8 @@
-use std::{fs::File, io};
-
-use io::BufReader;
+use std::fs::read_to_string;
 
 use crate::config::Config;
 use crate::project::find_files;
-use crate::todo_parser::TodoParser;
+use crate::todo_parser::find_todos;
 
 pub fn todo_files(conf: &Config) {
     let files = find_files(&conf.root, &conf.filter_fn);
@@ -17,18 +15,23 @@ pub fn todo_files(conf: &Config) {
 pub fn list_todos(conf: &Config, reported: bool, unreported: bool) {
     let files = find_files(&conf.root, &conf.filter_fn);
 
-    let parser = TodoParser::new(&conf.keywords, reported, unreported).unwrap();
+    for path in &files {
+        let input = read_to_string(path).unwrap();
+        let todos = find_todos(&conf.keywords, &input);
 
-    for path in files {
-        let file = File::open(&path).unwrap();
-        let buf = BufReader::new(file);
+        let relative = path.strip_prefix(&conf.root).unwrap();
 
-        if let Ok(todos) = parser.parse_file(buf) {
-            let relative = path.strip_prefix(&conf.root).unwrap();
-
-            for todo in todos {
-                println!("{}:{}", relative.display(), todo)
-            }
-        }
+        todos
+            .into_iter()
+            .filter(|t| {
+                if !unreported && t.issue_id.is_none() {
+                    false
+                } else if !reported && t.issue_id.is_some() {
+                    false
+                } else {
+                    true
+                }
+            })
+            .for_each(|t| println!("{}:{}", relative.display(), t));
     }
 }
--- src/todo_parser.rs
+++ src/todo_parser.rs
@@ -1,172 +1,87 @@
-use std::io::{self, BufRead};
-
 use regex::{escape, Regex};
 
 use crate::todo::Todo;
 
-pub struct TodoParser<'a> {
-    todo_regex: Regex,
-    comment_pattern: &'a str,
-}
+const COMMENT_PATTERN: &str = "^({})(?P<comment>.+)$";
 
-impl<'a> TodoParser<'a> {
-    pub fn new(
-        keywords: &Vec<String>,
-        reported: bool,
-        unreported: bool,
-    ) -> Result<Self, &'static str> {
-        if keywords.len() == 0 {
-            return Err("Must provide at least one keyword!");
-        }
+// Memory usage is probably high on big files
+// As the will be completly loaded into ram
+pub fn find_todos(keywords: &Vec<String>, input: &str) -> Vec<Todo> {
+    let regex = todo_regex(keywords);
+    let mut last_todo: Option<Todo> = None;
+    let mut output: Vec<Todo> = vec![];
 
-        Ok(Self {
-            todo_regex: TodoParser::create_todo_regex(keywords, reported, unreported)?,
-            comment_pattern: "^({})(?P<comment>.+)$",
-        })
-    }
+    for (line, text) in input.lines().enumerate() {
+        let mut parsed = parse_line(&regex, &text);
 
-    fn create_todo_regex(
-        keywords: &Vec<String>,
-        reported: bool,
-        unreported: bool,
-    ) -> Result<Regex, &'static str> {
-        let prefix = "(?P<prefix>.*)";
-        let keyword = format!("(?P<keyword>{})", keywords.join("|"));
-        let title = "(?P<title>.+)";
-        let issue_id = r"(\(#(?P<issue_id>\d+)\))";
+        if let Some(ref mut new_todo) = parsed {
+            if let Some(todo) = last_todo {
+                output.push(todo);
+            }
 
-        let todo_format = match (unreported, reported) {
-            (true, true) => format!("^{}{}{}?: {}$", prefix, keyword, issue_id, title),
-            (false, true) => format!("^{}{}{}: {}$", prefix, keyword, issue_id, title),
-            (true, false) => format!("^{}{}: {}$", prefix, keyword, title),
-            (false, false) => return Err("The pattern must be (un)-reported or both!"),
-        };
+            new_todo.line = line as u32 + 1;
+            last_todo = parsed;
+        } else if let Some(ref mut todo) = last_todo {
+            let reg = Regex::new(&COMMENT_PATTERN.replace("{}", &escape(&todo.prefix))).unwrap();
 
-        Ok(Regex::new(&todo_format).unwrap())
-    }
-
-    pub fn parse_file(&self, input: impl BufRead) -> io::Result<Vec<Todo>> {
-        let mut last_todo: Option<Todo> = None;
-        let mut output: Vec<Todo> = vec![];
-
-        for (line, text) in input.lines().enumerate() {
-            let text = text?;
-
-            let parsed = self.parse_line(line as u32 + 1, &text);
-
-            if parsed.is_some() {
-                if let Some(todo) = last_todo {
-                    output.push(todo);
-                }
-
-                last_todo = parsed;
-            } else if let Some(ref mut todo) = last_todo {
-                let reg =
-                    Regex::new(&self.comment_pattern.replace("{}", &escape(&todo.prefix))).unwrap();
-
-                if let Some(m) = reg.captures(&text) {
-                    todo.comments
-                        .push(m.name("comment").unwrap().as_str().to_owned())
-                }
+            if let Some(m) = reg.captures(&text) {
+                todo.comments
+                    .push(m.name("comment").unwrap().as_str().to_owned())
             }
         }
-
-        if let Some(todo) = last_todo {
-            output.push(todo)
-        }
-
-        Ok(output)
     }
 
-    fn parse_line(&self, line: u32, text: &str) -> Option<Todo> {
-        self.todo_regex.captures(&text).map(|c| Todo {
-            prefix: c.name("prefix").unwrap().as_str().to_owned(),
-            keyword: c.name("keyword").unwrap().as_str().to_owned(),
-            title: c.name("title").unwrap().as_str().to_owned(),
-            issue_id: c
-                .name("issue_id")
-                .map(|s| s.as_str().parse::<u32>().unwrap()),
-            comments: vec![],
-            line,
-        })
+    if let Some(todo) = last_todo {
+        output.push(todo)
     }
+
+    output
+}
+
+fn todo_regex(keywords: &Vec<String>) -> Regex {
+    let prefix = "(?P<prefix>.*)";
+    let keyword = format!("(?P<keyword>{})", keywords.join("|"));
+    let title = "(?P<title>.+)";
+    let issue_id = r"(\(#(?P<issue_id>\d+)\))";
+
+    Regex::new(&format!("^{}{}{}?: {}$", prefix, keyword, issue_id, title)).unwrap()
+}
+
+fn parse_line(regex: &Regex, text: &str) -> Option<Todo> {
+    regex.captures(&text).map(|c| Todo {
+        prefix: c.name("prefix").unwrap().as_str().to_owned(),
+        keyword: c.name("keyword").unwrap().as_str().to_owned(),
+        title: c.name("title").unwrap().as_str().to_owned(),
+        issue_id: c
+            .name("issue_id")
+            .map(|s| s.as_str().parse::<u32>().unwrap()),
+        comments: vec![],
+
+        // Line will be changed later
+        line: 0,
+    })
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    mod create_regex {
-        use super::*;
-
-        #[test]
-        fn create_regex_err() {
-            if let Err(message) = TodoParser::create_todo_regex(&Vec::new(), false, false) {
-                assert_eq!("The pattern must be (un)-reported or both!", message);
-            } else {
-                assert!(false)
-            }
-        }
-
-        #[test]
-        fn create_regex_keywords() {
-            let reg = TodoParser::create_todo_regex(
-                &vec!["TODO".to_owned(), "FIXME".to_owned()],
-                false,
-                true,
-            )
-            .unwrap();
-
-            assert_eq!(true, reg.is_match("// TODO: Something"));
-            assert_eq!(true, reg.is_match("// FIXME: Something"));
-            assert_eq!(false, reg.is_match("// BUG: Something"))
-        }
-
-        #[test]
-        fn create_regex_reported() {
-            let reg = TodoParser::create_todo_regex(
-                &vec!["TODO".to_owned(), "FIXME".to_owned()],
-                true,
-                false,
-            )
-            .unwrap();
-
-            assert_eq!(false, reg.is_match("// TODO: Something"));
-            assert_eq!(true, reg.is_match("// TODO(#123): Something"));
-        }
-
-        #[test]
-        fn create_regex_both() {
-            let reg = TodoParser::create_todo_regex(
-                &vec!["TODO".to_owned(), "FIXME".to_owned()],
-                true,
-                true,
-            )
-            .unwrap();
-
-            assert_eq!(true, reg.is_match("// TODO: Something"));
-            assert_eq!(true, reg.is_match("// TODO(#123): Something"));
-        }
-    }
-
     #[test]
-    fn new_parser_err() {
-        if let Err(message) = TodoParser::new(&Vec::new(), false, true) {
-            assert_eq!("Must provide at least one keyword!", message);
-        } else {
-            assert!(false)
-        }
+    fn create_regex() {
+        let reg = todo_regex(&vec!["TODO".to_owned(), "FIXME".to_owned()]);
+
+        assert_eq!(true, reg.is_match("// TODO: Something"));
+        assert_eq!(true, reg.is_match("// FIXME: Something"));
+        assert_eq!(false, reg.is_match("// BUG: Something"));
+        assert_eq!(true, reg.is_match("// TODO(#123): Something"))
     }
 
     mod parse_file {
-        use io::Cursor;
-
         use super::*;
 
         #[test]
         fn parse_one() {
-            let parser = TodoParser::new(&vec!["TODO".to_owned()], false, true).unwrap();
-            let input = Cursor::new("// TODO: Something");
+            let input = "// TODO: Something";
             let expected = Todo {
                 line: 1,
                 prefix: "// ".to_owned(),
@@ -176,13 +91,12 @@ mod tests {
                 comments: vec![],
             };
 
-            assert_eq!(vec![expected], parser.parse_file(input).unwrap())
+            assert_eq!(vec![expected], find_todos(&vec!["TODO".to_owned()], input))
         }
 
         #[test]
         fn parse_comments() {
-            let parser = TodoParser::new(&vec!["TODO".to_owned()], false, true).unwrap();
-            let input = Cursor::new("// TODO: Something\n// More\n// And more");
+            let input = "// TODO: Something\n// More\n// And more";
             let expected = Todo {
                 line: 1,
                 prefix: "// ".to_owned(),
@@ -192,13 +106,12 @@ mod tests {
                 comments: vec!["More".to_owned(), "And more".to_owned()],
             };
 
-            assert_eq!(vec![expected], parser.parse_file(input).unwrap())
+            assert_eq!(vec![expected], find_todos(&vec!["TODO".to_owned()], input))
         }
 
         #[test]
         fn parse_comment_escape() {
-            let parser = TodoParser::new(&vec!["TODO".to_owned()], false, true).unwrap();
-            let input = Cursor::new("// TODO: Something\n// More\n// And (\\d+) more");
+            let input = "// TODO: Something\n// More\n// And (\\d+) more";
             let expected = Todo {
                 line: 1,
                 prefix: "// ".to_owned(),
@@ -208,13 +121,12 @@ mod tests {
                 comments: vec!["More".to_owned(), r"And (\d+) more".to_owned()],
             };
 
-            assert_eq!(vec![expected], parser.parse_file(input).unwrap())
+            assert_eq!(vec![expected], find_todos(&vec!["TODO".to_owned()], input))
         }
 
         #[test]
         fn parse_issue_id() {
-            let parser = TodoParser::new(&vec!["TODO".to_owned()], true, true).unwrap();
-            let input = Cursor::new("// TODO(#42): Something");
+            let input = "// TODO(#42): Something";
             let expected = Todo {
                 line: 1,
                 prefix: "// ".to_owned(),
@@ -224,13 +136,12 @@ mod tests {
                 comments: vec![],
             };
 
-            assert_eq!(vec![expected], parser.parse_file(input).unwrap())
+            assert_eq!(vec![expected], find_todos(&vec!["TODO".to_owned()], input))
         }
 
         #[test]
         fn parse_mutiple() {
-            let parser = TodoParser::new(&vec!["TODO".to_owned()], false, true).unwrap();
-            let input = Cursor::new("// TODO: Something\n// More\n// TODO: Other\n// comment");
+            let input = "// TODO: Something\n// More\n// TODO: Other\n// comment";
             let expected_one = Todo {
                 line: 1,
                 prefix: "// ".to_owned(),
@@ -251,7 +162,7 @@ mod tests {
 
             assert_eq!(
                 vec![expected_one, expected_two],
-                parser.parse_file(input).unwrap()
+                find_todos(&vec!["TODO".to_owned()], input)
             )
         }
     }
